fc252fc0d5d163ae44497290e46bfd9175a4d12d
johnsonwangnzattourplan <johnsonwangnz@gmail.com>
2011-07-12 14:35:10 +1200

    ignor file changes

--- StockWatcherSpring/src/gbpring1/gwt/stockwatcher/client/StockPriceServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package gbpring1.gwt.stockwatcher.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface StockPriceServiceAsync {
-
-	 void getPrices(String[] symbols, AsyncCallback<StockPrice[]> callback);
-
-}
