54b1db492355d14b56cc9b055fb5dd62e0e7c804
johnsonwangnzattourplan <johnsonwangnz@gmail.com>
2011-07-13 16:59:33 +1200

    to implement gwt iserializable for delistedexception

--- StockWatcherSpring/src/gbpring1/gwt/stockwatcher/client/DelistedException.java
+++ StockWatcherSpring/src/gbpring1/gwt/stockwatcher/client/DelistedException.java
@@ -1,8 +1,8 @@
 package gbpring1.gwt.stockwatcher.client;
 
-import java.io.Serializable;
+import com.google.gwt.user.client.rpc.IsSerializable;
 
-public class DelistedException extends Exception implements Serializable{
+public class DelistedException extends Exception implements IsSerializable{
 
 	private String symbol;
 
