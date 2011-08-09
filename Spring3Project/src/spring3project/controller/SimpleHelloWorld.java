49d2febcb3c80162e932e6ea22de7928b28d1c50
johnsonwangnzattourplan <johnsonwangnz@gmail.com>
2011-08-09 16:50:47 +1200

    simplecontrollermapping

--- Spring3Project/src/spring3project/controller/SimpleHelloWorld.java
+++ Spring3Project/src/spring3project/controller/SimpleHelloWorld.java
@@ -3,16 +3,16 @@ package spring3project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HelloWorldController {
-	
-	@RequestMapping("/helloWorld")
+public class SimpleHelloWorld {
+
+	@RequestMapping(method=RequestMethod.GET)
 	public String helloWorld(Model model)
 	{
 		model.addAttribute("name","world");
 		return "hello/hello";
 	}
-
+	
 }
